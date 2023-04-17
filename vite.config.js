8808226846900593f935464cbfb6a11713253b01
Michael Sarpong-Boateng <info@sarppsthedev.com>
2023-04-17 19:14:33 +0100

    Addition to last commit

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: `/Notes_App`, //The base url of the host website
+  base: `/Notes_App/`, //The base url of the host website
   plugins: [react()],
 })
